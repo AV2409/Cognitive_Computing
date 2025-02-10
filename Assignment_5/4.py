03d1e4de9e528ab6bd09e4434b79dfccc3d89c6b
AV2409 <akshanshvij@gmail.com>
2025-02-10 20:33:12 +0530

    assignment-5

--- Assignment_5/4.py
+++ Assignment_5/4.py
@@ -2,19 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 100)
-y1 = x ** 2
-y2 = np.sin(x)
-y3 = np.exp(x)
-y4 = np.log(np.abs(x) + 1)
-
 plt.figure(figsize=(10, 6))
-plt.plot(x, y1, label='y = x^2')
-plt.plot(x, y2, label='y = sin(x)')
-plt.plot(x, y3, label='y = e^x')
-plt.plot(x, y4, label='y = log(|x| + 1)')
-plt.title("Function Plots")
-plt.xlabel("x values")
-plt.ylabel("y values")
-plt.legend()
-plt.grid()
+
+plt.subplot(2, 2, 1)
+plt.plot(x, x**2)
+plt.title("Y = x^2")
+plt.grid(True)
+
+plt.subplot(2, 2, 2)
+plt.plot(x, np.sin(x))
+plt.title("Y = sin(x)")
+plt.grid(True)
+
+plt.subplot(2, 2, 3)
+plt.plot(x, np.exp(x))
+plt.title("Y = e^x")
+plt.grid(True)
+
+plt.subplot(2, 2, 4)
+plt.plot(x, np.log(np.abs(x) + 1))
+plt.title("Y = log(|x| + 1)")
+plt.grid(True)
+
+plt.tight_layout()
 plt.show()
